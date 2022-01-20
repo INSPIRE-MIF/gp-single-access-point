4cc1b65332560291ba1e6bf07a6a3230dfbf803a
Marcin <mgrudzien7@gmail.com>
2022-01-20 13:17:32 +0100

    Add files via upload

---
+++ resources/software/wms_info.php
@@ -0,0 +1,13 @@
+<?php
+	if (isset($argv[1])) $what=$argv[1];
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1_strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>WMS</title>
+</head>
+<body>
+<p>Sample of WMS integration service</p>
+</body>
+</html>
